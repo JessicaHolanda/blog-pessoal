44b12a1e433a494ee810353e741b478112b8f163
Jessica Holanda <53666998+JessicaHolanda@users.noreply.github.com>
2021-04-07 11:12:30 -0300

    feat: build

--- src/main/java/generation/org/blogpessoal/model/UserLogin.java
+++ src/main/java/generation/org/blogpessoal/model/UserLogin.java
@@ -1,6 +1,8 @@
 package generation.org.blogpessoal.model;
 
 public class UserLogin {
+	
+	private long id;
 
 	private String nome;
 	
@@ -10,6 +12,20 @@ public class UserLogin {
 	
 	private String token;
 	
+	private String foto;
+	
+	private String tipo;
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
 	public String getNome() {
 		return nome;
 	}
@@ -42,4 +58,21 @@ public class UserLogin {
 		this.token = token;
 	}
 
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	
 }
